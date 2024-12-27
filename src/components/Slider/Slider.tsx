c6261bbeb43dcefed804169b2a9094155df2c348
Dinuka Dilshan <jadinukadilshan@gmail.com>
2024-12-27 23:36:41 +0530

    fix

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import "swiper/css";
 
-// Import Swiper styles
-import 'swiper/swiper.min.css';
 
 type Props<T> = {
   list: T[];
