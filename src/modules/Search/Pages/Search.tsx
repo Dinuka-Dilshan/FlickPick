b2a03bb105602daaabcd1b88e08fafa531dc4e83
Dinuka Dilshan <jadinukadilshan@gmail.com>
2024-12-28 09:47:37 +0530

    fix

--- src/modules/Search/Pages/Search.tsx
+++ src/modules/Search/Pages/Search.tsx
@@ -1,6 +1,6 @@
-import { Box, Grid2, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
-import LoadingItemIndicator from "../../../components/LoadingItemIndicator/LoadingItemIndicator";
+import ItemListLayout from "../../../components/Layouts/ItemListLayout";
 import MovieCard from "../../../components/MovieCard/MovieCard";
 import SearchInput from "../../../components/SearchInput/SearchInput";
 import { QUERY_KEYS } from "../../../constants/queryKeys";
@@ -24,7 +24,7 @@ const Search = () => {
     value: searchText,
   });
 
-  const { data, isFetching } = useAppQuery<
+  const { data, isFetching, error } = useAppQuery<
     SearchMovieItem,
     Error,
     SearchMovieResponse
@@ -35,41 +35,36 @@ const Search = () => {
   });
 
   return (
-    <Box>
-      {isLargeScreen ? (
-        <Typography sx={{ color: "#E7E7E7" }}>
-          Show results for{" "}
-          <span style={{ fontWeight: "bold" }}>"{searchText}"</span>
-        </Typography>
-      ) : (
-        <SearchInput />
+    <ItemListLayout
+      title={
+        isLargeScreen ? (
+          <Typography sx={{ color: "#E7E7E7" }}>
+            Show results for{" "}
+            <span style={{ fontWeight: "bold" }}>"{searchText}"</span>
+          </Typography>
+        ) : (
+          <SearchInput />
+        )
+      }
+      emptyMessage={{
+        show: true,
+        actionLabel: "",
+        message:
+          "Oops! It seems we couldn't find any matches. Maybe the movie you're looking for is still in production? 🎥🍿",
+        action: () => null,
+      }}
+      error={error}
+      isLoading={isFetching}
+      itemRenderer={(movie) => (
+        <MovieCard movie={movie}>
+          <MovieCard.TitleContainer>
+            <MovieCard.Title />
+            <MovieCard.ReleaseYear />
+          </MovieCard.TitleContainer>
+        </MovieCard>
       )}
-
-      <LoadingItemIndicator
-        isLoading={isFetching}
-        itemCount={10}
-        itemsPerRow={{
-          xs: 2,
-          md: 3,
-          lg: 5,
-        }}
-        itemHeight={{ xs: 250, md: 300, lg: 350 }}
-      />
-      {!isFetching && (
-        <Grid2 container spacing={1.5} mt={"1rem"}>
-          {data?.map((movie, index) => (
-            <Grid2 size={{ xs: 6, md: 4, lg: 12 / 5 }} key={index}>
-              <MovieCard movie={movie}>
-                <MovieCard.TitleContainer>
-                  <MovieCard.Title />
-                  <MovieCard.ReleaseYear />
-                </MovieCard.TitleContainer>
-              </MovieCard>
-            </Grid2>
-          ))}
-        </Grid2>
-      )}
-    </Box>
+      itemList={data}
+    />
   );
 };
 
